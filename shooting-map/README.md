33da32216afc3f92463ff2e38982bf476d09b279
gmanci <gmanci@uw.edu>
2015-10-20 01:30:49 -0700

    pushing correct files

--- shooting-map/README.md
+++ shooting-map/README.md
@@ -1,19 +1,19 @@
 # Map Challenge: Police Shootings
 
-This folder contains your submission for the [map challenge](http://faculty.washington.edu/joelross/courses/info343/#/challenges/map). The below questions should be answered with details about your submission!
+This folder contains your submission for the [map challenge](http://info343-joelross.rhcloud.com/challenges/map). The below questions should be answered with details about your submission!
 
 ##### 1. What information does your visualization show? What 'argument' are you making? #####
-> Answer goes here!
+It shows the numbers of armed and unarmed victims of police shootings along with summaries of the events. Unarmed victims show in a more noticible, darker color to bring attention to the situations that most likely could have been handled in a less violent manner.
 
 ##### 2. Did you complete any advanced extensions to this challenge? If so, what? #####
-> Answer goes here!
+n/a
 
 ##### 3. Did you receive any help from others (classmates, etc)? If so, please list who. #####
-> Answer goes here!
+I collaborated a couple of times on this project with Halle Friedman, Quinn Mau and Casey Lee.
 
 ##### 4. Approximately how many hours did it take you to complete this challenge? #####
-> Answer goes here!
+~12 hr
 
 ##### 5. Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better? #####
-> Answer goes here!
+I think this challenge needs more explanation on how to associate information between the HTML and JS files.
 
--- shooting-map/js/app.js
+++ shooting-map/js/app.js
@@ -1,4 +1,58 @@
 /* add your script methods and logic here */
 
-'use strict';
+'use strict'; 
+
+
+/* map variable*/
+var map = L.map('map');
+
+/* view set to America*/
+map.setView([38.8, -98], 4);
+
+/* creates map*/
+var layer = L.tileLayer('https://api.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ21hbmNpIiwiYSI6ImNpZnZpMTN1dzIyeTl1NmtzM2U2eWpxb3QifQ.JywvFvRbnpbGVCwSPvXrlw', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'gmanci.cifvi12hk3fszoekr93zqqnrf',
+    accessToken: 'pk.eyJ1IjoiZ21hbmNpIiwiYSI6ImNpZnZpMTN1dzIyeTl1NmtzM2U2eWpxb3QifQ.JywvFvRbnpbGVCwSPvXrlw'
+}).addTo(map);
+
+/* declares a weapon category using w/ and w/o weapons */
+var withWeapon = L.layerGroup([]);
+var withoutWeapon = L.layerGroup([]);
+var weapons = {
+    "Victims W/ Weapon": withWeapon,
+    "Victims W/O Weapon": withoutWeapon
+};
+var withCounter = 0;
+var withoutCounter = 0;
+
+/* function used to place different colored points on map with summaries that show once clicked */
+var plotPoints = function(data) {
+    for (var i = 0; i < data.length; i++) {
+        var summary = data[i].summary;
+        if (data[i].armed == true){
+            var marker = L.circle([data[i].lat, data[i].lng], 500, {
+                color: 'DarkCyan'
+            }).bindPopup(summary).addTo(withWeapon);
+            withCounter++;
+        } else {
+            var marker = L.circle([data[i].lat, data[i].lng], 500, {
+                color: 'MidnightBlue'
+            }).bindPopup(summary).addTo(withoutWeapon);
+            withoutCounter++;
+        }
+    }
+    console.log(withCounter, withoutCounter);
+}
+
+/* makes small control panel to toggle showing armed and unarmed victims*/
+L.control.layers(null, weapons).addTo(map);
+
+/* makes sure data is loaded before points are plotted on map*/
+$.getJSON('data/data.min.json').then(plotPoints);
+
+
+
+
 
