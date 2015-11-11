0570c66c645e01c8c8a18e78cc860963a634e36c
gmanci <gmanci@uw.edu>
2015-11-11 14:53:43 -0800

    added bean pages

--- dawg-coffee/js/app.js
+++ dawg-coffee/js/app.js
@@ -33,9 +33,15 @@ angular.module('myApp', ['ngSanitize', 'ui.router'])
           url: "/home",
           templateUrl: "partials/home.html"
         })
-        .state('order.bean', {
-          url: "#/order#bean",
-          templateUrl: "partials/bean.html"
+//        .state('detail', {
+//			url: '/movie/{id}',
+//			templateUrl: 'partials/movie-detail.html',
+//			controller: 'DetailCtrl'
+//		})
+        .state('detail', {
+          url: "/bean/{id}",
+          templateUrl: "partials/bean.html",
+          controller: 'BeanCtrl'
         })
         .state('order', {
           url: "/order",
@@ -53,15 +59,15 @@ angular.module('myApp', ['ngSanitize', 'ui.router'])
  		$scope.beans = response.data;
  	});
 
+}])
+
+//For details view
+.controller('BeanCtrl', ['$scope', '$http', '$stateParams', '$filter', function($scope, $http, $stateParams, $filter) {
+
+	$http.get('data/products.json').then(function(response) {
+   	$scope.bean = $filter('filter')(response.data, { //filter the array
+      id: $stateParams.id //for items whose id property is targetId
+   	}, true)[0]; //save the 0th result
+ 	});
 }]);
-//
-////For movie data browser
-//.controller('OrderCtlr', ['$scope', '$http', function($scope, $http) {
-//
-//	$scope.sortingCriteria = '';
-//
-//	$http.get('data/products.json').then(function(response) {
-// 		$scope.movies = response.data;
-// 	});
-//
-//}]);
+
