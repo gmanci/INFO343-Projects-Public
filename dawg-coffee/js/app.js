66d8372fee0966909ed4016702453d14a5f33d8c
gmanci <gmanci@uw.edu>
2015-11-11 16:11:49 -0800

    added addToCart form

--- dawg-coffee/js/app.js
+++ dawg-coffee/js/app.js
@@ -61,13 +61,27 @@ angular.module('myApp', ['ngSanitize', 'ui.router'])
 
 }])
 
+
+
 //For details view
 .controller('BeanCtrl', ['$scope', '$http', '$stateParams', '$filter', function($scope, $http, $stateParams, $filter) {
-
+    
+    $scope.grinds = ['Whole Bean', 'Espresso', 'French Press', 'Cone Drip', 'Flat Bottom'];
+    $scope.grind = 'Whole Bean'; //default
+    $scope.quantities = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+    $scope.quantity = '1'; //default
+    
 	$http.get('data/products.json').then(function(response) {
    	$scope.bean = $filter('filter')(response.data, { //filter the array
       id: $stateParams.id //for items whose id property is targetId
    	}, true)[0]; //save the 0th result
  	});
+    
+    $scope.addToCart = function(bean, grind, quantity){
+    bean.grind = $scope.grinds.indexOf(grind);
+    bean.quantity = $scope.quantities.indexOf(quantity);
+    watchListService.saveFilm(movie);
+}
+    
 }]);
 
