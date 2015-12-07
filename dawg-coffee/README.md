224392d09a5855068148d75c2c2886947f006668
gmanci <gmanci@uw.edu>
2015-12-07 15:58:26 -0800

    reordered javascript

--- dawg-coffee/README.md
+++ dawg-coffee/README.md
@@ -3,36 +3,15 @@
 *Update!* This folder contains your submission for the [shopping cart challenge](http://info343-joelross.rhcloud.com/challenges/cart). Please answer the below questions with details about your submission for this later challenge.
 
 ##### 1. Did you receive any help from others (classmates, etc)? If so, please list who. #####
-> Answer goes here!
-
-##### 2. Did you complete any advanced extensions to this challenge? If so, what? #####
-> Answer goes here!
-
-##### 3. Approximately how many hours did it take you to complete this challenge? #####
-> Answer goes here!
-
-##### 4. Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better? #####
-> Answer goes here!
-
-
-
-
-***
-
-# Responsive Challenge: Dawg Coffee
-
-This folder contains your submission for the [responsive challenge](http://info343-joelross.rhcloud.com/challenges/responsive). The below questions should be answered with details about your submission!
-
-
-##### 1. Did you receive any help from others (classmates, etc)? If so, please list who. #####
-> I discussed this assignment a lot with Quinn Mau, a student that attends your INFO 343 B section but has not yet been given an add code.
+> n/a
 
 ##### 2. Did you complete any advanced extensions to this challenge? If so, what? #####
 > n/a
 
 ##### 3. Approximately how many hours did it take you to complete this challenge? #####
-> easily 24 hours or more. I made a lot of simple mistakes that I got caught on for a very long time.
+> ~10
 
 ##### 4. Did you encounter any problems in this challenge we should warn students about in the future? How can we make the challenge better? #####
-> I set myself way back because I improperly imported Bootstrap. Maybe I just made a stupid mistake, but you could make that part of the instructions a little more clear.
+> n/a
+
 
--- dawg-coffee/js/app.js
+++ dawg-coffee/js/app.js
@@ -1,8 +1,83 @@
 'use strict';
 
 
-angular.module('myApp', ['ngSanitize', 'ui.router'])
-    .config(function($stateProvider) {
+var myApp = angular.module('dawgApp', ['ngSanitize', 'ui.router'])
+
+    .factory('myService', function(){
+
+        var service = {};
+
+        service.list = [];
+
+        service.saveBean = function(bean){
+            console.log("adding bean to cart");
+            service.list.push(bean);
+        };
+
+        return service;
+    })
+
+
+
+    .controller('CoffeeCtlr', ['$scope', '$http', 'myService', function($scope, $http){
+
+        $scope.sortingCriteria = '';
+
+        $http.get('data/products.json').then(function(response) {
+            $scope.beans = response.data;
+        });
+
+        $scope.saveBean = function(bean){
+            bean.Name = bean.name;
+            bean.Grind = 'Whole Bean';
+            movie.quantity = '1';
+            myService.saveBean(bean);	
+        } 
+    }])
+
+
+
+    //For details view
+    .controller('BeanCtrl', ['$scope', '$http', '$stateParams', '$filter', 'myService', function($scope, $http, $stateParams, $filter) {
+
+        $scope.grinds = ['Whole Bean', 'Espresso', 'French Press', 'Cone Drip', 'Flat Bottom'];
+        $scope.grind = 'Whole Bean'; //default
+        $scope.quantities = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
+        $scope.quantity = '1'; //default
+
+
+
+        $http.get('data/products.json').then(function(response) {
+        $scope.bean = $filter('filter')(response.data, { //filter the array
+          id: $stateParams.id //for items whose id property is targetId
+        }, true)[0]; //save the 0th result
+        });
+
+        $scope.addToCart = function(bean, grind, quantity){
+            bean.grind = $scope.grinds.indexOf(grind);
+            bean.quantity = $scope.quantities.indexOf(quantity);
+            myService.saveBean(bean);
+        }
+
+    }])
+
+    .controller('CartCtlr', ['$scope', '$http', '$uibModal', 'myService', function($scope, $http, $uibModal, watchListService) {
+
+        $scope.cartList = myService.list;
+
+        $scope.saveBean = function(bean, grind, quantity){
+            bean.grind =   $scope.grinds.indexOf(grind);
+            bean.quantity =   $scope.quantities.indexOf(quantity);
+            myService.saveBean(bean);
+        }
+
+
+    }])
+
+    .config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise("/");
+    
       $stateProvider
         .state('home', {
           url: "/home",
@@ -23,94 +98,7 @@ angular.module('myApp', ['ngSanitize', 'ui.router'])
           url: "/order",
           templateUrl: "partials/order.html"
         });
-    })
+    });
 
 
-
-.controller('CoffeeCtlr', ['$scope', '$http', 'myService', function($scope, $http){
-
-	$scope.sortingCriteria = '';
-
-	$http.get('data/products.json').then(function(response) {
- 		$scope.beans = response.data;
- 	});
-    
-    $scope.saveBean = function(bean){
- 		bean.Name = bean.name;
- 		bean.Grind = 'Whole Bean';
- 		movie.quantity = '1';
- 		myService.saveBean(bean);	
- 	} 
-
-}])
-
-
-
-//For details view
-.controller('BeanCtrl', ['$scope', '$http', '$stateParams', '$filter', 'myService', function($scope, $http, $stateParams, $filter) {
-    
-    $scope.grinds = ['Whole Bean', 'Espresso', 'French Press', 'Cone Drip', 'Flat Bottom'];
-    $scope.grind = 'Whole Bean'; //default
-    $scope.quantities = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-    $scope.quantity = '1'; //default
-    
-    
-    
-	$http.get('data/products.json').then(function(response) {
-   	$scope.bean = $filter('filter')(response.data, { //filter the array
-      id: $stateParams.id //for items whose id property is targetId
-   	}, true)[0]; //save the 0th result
- 	});
-    
-    $scope.addToCart = function(bean, grind, quantity){
-    bean.grind = $scope.grinds.indexOf(grind);
-    bean.quantity = $scope.quantities.indexOf(quantity);
-    myService.saveBean(bean);
-}
-    
-}])
-
-.controller('CartCtlr', ['$scope', '$http', '$uibModal', 'myService', function($scope, $http, $uibModal, watchListService) {
-
-//	//"constants" for priority setting
-//	$scope.priorities = ['Very High', 'High', 'Medium', 'Low', 'Very Low'];
-//	$scope.priority = 'Medium'; //default
-
-	$scope.cartList = myService.list;
-
-	$scope.saveBean = function(bean, grind, quantity){
-		bean.grind =   $scope.grinds.indexOf(grind);
-        bean.quantity =   $scope.quantities.indexOf(quantity);
-		myService.saveBean(bean);
-	}
-
-
-}])
-
-.factory('myService', function(){
-
-	var service = {};
-
-	service.list = [];
-
-	service.saveBean = function(bean){
-		console.log("adding bean to cart");
-		service.list.push(bean);
-	};
-
-	return service;
-});
-
-//myApp.factory('myService', function() {
-//
-//  var service = {}; //object that is the service
-//
-//  //can store data in the service
-//  service.message = "Hello world";
-//
-//  //can store fuctions as well!
-//  service.myFunc = function() { ... };
-//
-//  return service; //return ("build") that service
-//})
 
