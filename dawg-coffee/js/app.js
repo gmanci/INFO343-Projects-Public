da4cb821ead61e513cd320863b4c462ed7f04f34
gmanci <gmanci@uw.edu>
2015-12-14 12:04:19 -0800

    testing redirect

--- dawg-coffee/js/app.js
+++ dawg-coffee/js/app.js
@@ -4,7 +4,7 @@
 var myApp = angular.module('dawgApp', ['ngSanitize', 'ui.router', 'firebase']);
 
 myApp.config(function($stateProvider, $urlRouterProvider) {
-        $urlRouterProvider.otherwise("/");
+        $urlRouterProvider.otherwise("/home#info");
         $stateProvider
         .state('home', {
           url: "/home",
